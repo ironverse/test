c539429186b30fc0073f490097fe988153f17978
ashneverdawn <ashneverdawn@gmail.com>
2019-12-16 22:45:27 -0500

    terrain genration test

---
+++ generate_terrain.js
@@ -0,0 +1,27 @@
+
+Iron.V0.SetTerrainGenerator("terrain_generator")
+function terrain_generator (seed, chunkRadius, data, offsetX, offsetY, offsetZ) {
+    Iron.V0.NewOpenSimplex(seed)
+
+    var frequency = 0.025
+    var heightRange = 50.0
+
+    var i = 0
+    for(var x = -chunkRadius; x <= chunkRadius; x++) {
+        for(var z = -chunkRadius; z <= chunkRadius; z++) {
+            if(Math.abs(x-z) <= chunkRadius) {
+                for(var y = -chunkRadius; y <= chunkRadius; y++) {
+                    
+                    data[i] = 0
+                    var value = Iron.V0.OpenSimplexEval2((offsetX+x) * frequency, (offsetZ+z) * frequency)
+                    var height = (value + 1) * 0.5 * heightRange
+                    if(offsetY+y <= height) {
+                        data[i] = 1
+                    }
+
+                    i++
+                }
+            }
+        }
+    }
+}
