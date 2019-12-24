31a46c800e779b19982d687af899fa0fba87ebdc
ashneverdawn <ashneverdawn@gmail.com>
2019-12-24 02:53:06 -0500

    tweak caves

--- generate_terrain.go
+++ generate_terrain.go
@@ -43,7 +43,7 @@ func GenerateWorld(data []byte, chunkRadius int, chunk *core.Position, hexOffset
 					caveY := float64(hexOffset.Y+y) * CaveFrequency
 					caveZ := float64(hexOffset.Z+z) * CaveFrequency
 					value = core.WorldGen.Get3dNoise(caveX, caveY, caveZ)
-					if value > 0.45 {
+					if value > 0.55 {
 						data[i] = 0
 					}
 
